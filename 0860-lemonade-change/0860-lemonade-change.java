102c222046eeff4a3f215a263d18d193afc32e71
Ishani Sharma <ishanisharma3101@gmail.com>
2026-07-06 11:18:48 +0530

    Time: 2 ms (99.3%), Space: 72.5 MB (29.22%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-
-
     int five =0;
     int ten =0;
     int twenty =0;
@@ -12,7 +10,6 @@ class Solution {
             else {
                 five--;
               ten++;}
-
         }
         else{
           if (ten >= 1 && five >= 1) {
@@ -29,6 +26,7 @@ else {
 }
 
     }
+    
     }
     return true;
     }
