0f2291d5265f6077a06cb3b20bd29dd493da2bd5
Ishani Sharma <ishanisharma3101@gmail.com>
2026-07-06 16:56:35 +0530

    Time: 21 ms (91.57%), Space: 83 MB (63.69%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,49 +1,53 @@
 class Solution {
 
     public int[] sortArray(int[] nums) {
+    // BASICALLY HEAP SORT QUES :--
 
-        int n = nums.length;
+    //     int n = nums.length;
 
-        // Build Max Heap
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(nums, n, i);
-        }
+    //     // Build Max Heap
+    //     for (int i = n / 2 - 1; i >= 0; i--) {
+    //         heapify(nums, n, i);
+    //     }
 
-        // One by one move current root to end
-        for (int i = n - 1; i > 0; i--) {
+    //     // One by one move current root to end
+    //     for (int i = n - 1; i > 0; i--) {
 
-            // Swap root with last element
-            int temp = nums[0];
-            nums[0] = nums[i];
-            nums[i] = temp;
+    //         // Swap root with last element
+    //         int temp = nums[0];
+    //         nums[0] = nums[i];
+    //         nums[i] = temp;
 
-            // Heapify remaining heap
-            heapify(nums, i, 0);
-        }
+    //         // Heapify remaining heap
+    //         heapify(nums, i, 0);
+    //     }
 
-        return nums;
-    }
+    //     return nums;
+    // }
 
-    public void heapify(int[] arr, int n, int i) {
+    // public void heapify(int[] arr, int n, int i) {
 
-        int largest = i;
+    //     int largest = i;
 
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
+    //     int left = 2 * i + 1;
+    //     int right = 2 * i + 2;
 
-        if (left < n && arr[left] > arr[largest])
-            largest = left;
+    //     if (left < n && arr[left] > arr[largest])
+    //         largest = left;
 
-        if (right < n && arr[right] > arr[largest])
-            largest = right;
+    //     if (right < n && arr[right] > arr[largest])
+    //         largest = right;
 
-        if (largest != i) {
+    //     if (largest != i) {
 
-            int temp = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = temp;
+    //         int temp = arr[i];
+    //         arr[i] = arr[largest];
+    //         arr[largest] = temp;
 
-            heapify(arr, n, largest);
-        }
+    //         heapify(arr, n, largest);
+    //     }
+
+    Arrays.sort(nums);
+    return nums;
     }
 }
