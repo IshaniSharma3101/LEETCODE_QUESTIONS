ab8450785f95a85f3b93594c1996225ed260e0b2
Ishani Sharma <ishanisharma3101@gmail.com>
2026-07-06 17:51:35 +0530

    Time: 8 ms (14.35%), Space: 46.5 MB (62.65%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
         // Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-
+        // understand these comparator !!! they are so so so important 
         Arrays.sort(intervals, (a,b) -> {
     if(a[0] == b[0])
         return b[1] - a[1];
