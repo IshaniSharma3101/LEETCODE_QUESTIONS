af13b1ebd74ca7521f7c88ea57f4060ec54c6288
Ishani Sharma <ishanisharma3101@gmail.com>
2026-07-08 08:44:41 +0530

    Time: 1 ms (99.85%), Space: 42.3 MB (96.51%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -12,3 +12,25 @@ long sum = 0;
 
     }
 }
+
+// without data structure :- ( vahi 2 loop ka concept easy tha bro )
+// class Solution {
+//     public long sumAndMultiply(int n) {
+//         int x=0;
+//         while(n>0){
+//             int digit = n%10;
+//             if(digit != 0)
+//                 x= (10*x)+digit;
+//             n/=10;
+//         }
+//         long rev=0;
+//         long sum=0;
+//         while(x>0){
+//             int dig = x%10;
+//             rev = (10*rev)+dig;
+//             sum += dig;
+//             x/=10;
+//         }
+//         return rev*sum;
+//     }
+// }
