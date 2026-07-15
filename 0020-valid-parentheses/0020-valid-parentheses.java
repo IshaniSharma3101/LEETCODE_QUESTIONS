6ce01492496254725759cf7d8eb4ae96c52f52d8
Ishani Sharma <ishanisharma3101@gmail.com>
2026-07-15 16:59:23 +0530

    Time: 2 ms (98.27%), Space: 43.2 MB (58.23%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] arr= new char[s.length()];
+        int index=0;
+        for(int i=0; i <s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{') arr[index++] = s.charAt(i); 
+
+            else{
+                if(index==0) return false;
+                if((s.charAt(i)==')' && arr[index-1]!='(' ) ||(s.charAt(i)=='}' && arr[index-1]!='{' ) || (s.charAt(i)==']' && arr[index-1]!='[' ) ) return false;
+                index--;
+            }
+        }
+        return index==0;
+    }
+}
